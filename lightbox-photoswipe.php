246d1f5436acd16a33fe2b2966db9a11b85ef77a
Arno Welzel <github@arnowelzel.de>
2018-12-17 20:50:20 +0100

    Remove grey image placeholder when opening the lightbox.

--- lightbox-photoswipe.php
+++ lightbox-photoswipe.php
@@ -3,7 +3,7 @@
 Plugin Name: Lightbox with PhotoSwipe
 Plugin URI: https://wordpress.org/plugins/lightbox-photoswipe/
 Description: Lightbox with PhotoSwipe
-Version: 1.82
+Version: 1.83
 Author: Arno Welzel
 Author URI: http://arnowelzel.de
 Text Domain: lightbox-photoswipe
@@ -17,7 +17,7 @@ defined('ABSPATH') or die();
  */
 class LightboxPhotoSwipe
 {
-    const LIGHTBOX_PHOTOSWIPE_VERSION = '1.82';
+    const LIGHTBOX_PHOTOSWIPE_VERSION = '1.83';
     var $disabled_post_ids;
     var $share_facebook;
     var $share_pinterest;
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Contributors: awelzel
 Tags: attachments, images, gallery, lightbox, fancybox, photoswipe
 Requires at least: 4.0
 Tested up to: 5.0
-Stable tag: 1.82
+Stable tag: 1.83
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -93,6 +93,10 @@ To avoid any confusion: this plugin was published with the agreement of Dmitry S
 2. Example for the use in the frontend
 
 == Changelog ==
+
+= 1.83 =
+
+* Removed visible grey placeholder when opening the lightbox.
 
 = 1.82 =
 
