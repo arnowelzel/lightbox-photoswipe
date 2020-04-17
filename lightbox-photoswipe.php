9b33b5e9f620b6be00e08d9e5dd791f69f4aba07
Arno Welzel <github@arnowelzel.de>
2020-04-18 01:18:39 +0200

    Bump version for next release.

--- lightbox-photoswipe.php
+++ lightbox-photoswipe.php
@@ -3,7 +3,7 @@
 Plugin Name: Lightbox with PhotoSwipe
 Plugin URI: https://wordpress.org/plugins/lightbox-photoswipe/
 Description: Lightbox with PhotoSwipe
-Version: 2.90
+Version: 2.91
 Author: Arno Welzel
 Author URI: http://arnowelzel.de
 Text Domain: lightbox-photoswipe
@@ -19,7 +19,7 @@ require_once ABSPATH . '/wp-admin/includes/image.php';
  */
 class LightboxPhotoSwipe
 {
-    const LIGHTBOX_PHOTOSWIPE_VERSION = '2.90';
+    const LIGHTBOX_PHOTOSWIPE_VERSION = '2.91';
     var $disabled_post_ids;
     var $share_facebook;
     var $share_pinterest;
