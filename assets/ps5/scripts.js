d84c0d53897770da7b8e7b4f7333a7c94dae6791
Arno Welzel <github@arnowelzel.de>
2022-06-14 16:13:44 +0000

    Adjust fullscreen icons and remove code example

--- assets/ps5/scripts.js
+++ assets/ps5/scripts.js
@@ -312,9 +312,15 @@ let lbwpsInit = function(domUpdate) {
             });
         }
 
-        // Add fullscreen button
+        // Add fullscreen button and keyboard shortcut for fullscreen mode
+        // based on the idea described at https://github.com/dimsemenov/PhotoSwipe/issues/1759
         if (lbwpsOptions.show_fullscreen === '1') {
-            const fullscreenSVG = '<svg aria-hidden="true" class="pswp__icn" viewBox="0 0 32 32" width="32" height="32"><use class="pswp__icn-shadow" xlink:href="#pswp__icn-fullscreen-exit"/><use class="pswp__icn-shadow" xlink:href="#pswp__icn-fullscreen-request"/><path d="M8 8v6.047h2.834v-3.213h3.213V8h-3.213zm9.953 0v2.834h3.213v3.213H24V8h-2.834zM8 17.953V24h6.047v-2.834h-3.213v-3.213zm13.166 0v3.213h-3.213V24H24v-6.047z" id="pswp__icn-fullscreen-request"/><path d="M11.213 8v3.213H8v2.834h6.047V8zm6.74 0v6.047H24v-2.834h-3.213V8zM8 17.953v2.834h3.213V24h2.834v-6.047h-2.834zm9.953 0V24h2.834v-3.213H24v-2.834h-3.213z" id="pswp__icn-fullscreen-exit" style="display:none"/></svg>';
+            const fullscreenSVG = '<svg aria-hidden="true" class="pswp__icn" viewBox="0 0 32 32" width="32" height="32">' +
+                '<use class="pswp__icn-shadow" xlink:href="#pswp__icn-fullscreen-exit"/>' +
+                '<use class="pswp__icn-shadow" xlink:href="#pswp__icn-fullscreen-request"/>' +
+                '<path id="pswp__icn-fullscreen-request" transform="translate(4,4)" d="M20 3h2v6h-2V5h-4V3h4zM4 3h4v2H4v4H2V3h2zm16 16v-4h2v6h-6v-2h4zM4 19h4v2H2v-6h2v4z" /></g>' +
+                '<path id="pswp__icn-fullscreen-exit" style="display:none" transform="translate(4,4)" d="M18 7h4v2h-6V3h2v4zM8 9H2V7h4V3h2v6zm10 8v4h-2v-6h6v2h-4zM8 15v6H6v-4H2v-2h6z"/>' +
+                '</svg>';
 
             lightbox.on('uiRegister', function() {
                 lightbox.pswp.ui.registerElement({
@@ -343,7 +349,7 @@ let lbwpsInit = function(domUpdate) {
             });
         }
 
-        // Experimental slide transition
+        // Experimental slide transition - does not work very well with endless rotation
         /*
         const customGoTo = (index, animate = false) => {
             const ctx = lightbox.pswp;
