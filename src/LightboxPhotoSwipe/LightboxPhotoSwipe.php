9d636f26c47358e616dc9a4e581b7158e55a216f
Arno Welzel <github@arnowelzel.de>
2022-07-15 23:48:38 +0000

    Fix uninstall hook and remove unneccessary code.

--- src/LightboxPhotoSwipe/LightboxPhotoSwipe.php
+++ src/LightboxPhotoSwipe/LightboxPhotoSwipe.php
@@ -745,15 +745,10 @@ class LightboxPhotoSwipe
             $blog_ids = $wpdb->get_col('SELECT blog_id FROM '.$wpdb->blogs);
             foreach ($blog_ids as $blog_id) {
                 switch_to_blog($blog_id);
-                $this->deleteDatabaseTables();
                 $optionsManager->deleteOptions();
-                wp_clear_scheduled_hook('lbwps_cleanup');
                 restore_current_blog();
             }
         } else {
-            lightboxPhotoswipeDeleteTables();
-            wp_clear_scheduled_hook('lbwps_cleanup');
-            $this->deleteDatabaseTables();
             $optionsManager->deleteOptions();
         }
     }
